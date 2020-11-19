1e42afaf57847b9bb3225177f9e6b21f683edb62
kimtaru2 <kimtaru2@gmail.com>
2020-11-19 11:32:47 +0900

    h2 db에 데이터 입출력, json viewer 설치

---
+++ src/main/java/com/workroom/springboot/service/posts/PostsService.java
@@ -0,0 +1,32 @@
+package com.workroom.springboot.service.posts;
+
+import com.workroom.springboot.domain.posts.Posts;
+import com.workroom.springboot.domain.posts.PostsRepository;
+import com.workroom.springboot.web.dto.PostsResponseDto;
+import com.workroom.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsSaveRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다 id"+id));
+        posts.update(requestDto.getTitle(), requestDto.getTitle());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        return new PostsResponseDto(entity);
+    }
+}
