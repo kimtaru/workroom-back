52de2df4e67a8568aaa57dca2f1670b53232d631
jinsol <kimtaru2@gmail.com>
2020-11-29 21:53:28 +0900

    issue 관련 삭제

--- src/main/java/com/workroom/springboot/domain/issues/IssuesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.workroom.springboot.domain.issues;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface IssuesRepository extends JpaRepository<Issues, Long> {
-
-}
